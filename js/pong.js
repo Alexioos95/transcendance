00d9fe5ee73a2c4d61ba7d5d80f4248bb8b791ab
Alexis PAYEN <apayen@bess-f1r5s6.clusters.42paris.fr>
2024-05-23 16:20:15 +0200

    Working rebounds. Left: Corners.

--- js/pong.js
+++ js/pong.js
@@ -4,68 +4,46 @@ function getCanevas()
 	return (canvas);
 }
 
-function createPaddle(canvas, position)
+function createPaddle(canvas, height, width, position)
 {
 	let paddle = {
-		height: canvas.height / 5,
-		width: canvas.width / 50,
 		x: 0,
 		y: 0,
-		speed: 7,
 		move_top: 0,
 		move_bot: 0,
 	}
 	if (position == "l")
 	{
 		paddle.x = canvas.width / 20;
-		paddle.y = (canvas.height / 2) - (paddle.height / 2)
+		paddle.y = (canvas.height / 2) - (height / 2)
 	}
 	else if (position == "r")
 	{
-		paddle.x = canvas.width - (canvas.width / 20) - paddle.width;
-		paddle.y = (canvas.height / 2) - (paddle.height / 2);
+		paddle.x = canvas.width - (canvas.width / 20) - width;
+		paddle.y = (canvas.height / 2) - (height / 2);
 	}
 	return (paddle);
 }
 
 function getPaddles(canvas)
 {
+	let height = canvas.height / 5;
+	let width = canvas.width / 50;
 	let paddles = {
-		left: createPaddle(canvas, "l"),
-		right: createPaddle(canvas, "r")
+		height: height,
+		width: width,
+		speed: 8,
+		left: createPaddle(canvas, height, width, "l"),
+		right: createPaddle(canvas, height, width, "r")
 	}
 	return (paddles);
 }
 
-function getBall(canvas)
-{
-	const ball = {
-		radius: 20,
-		x: canvas.width / 2,
-		y: canvas.height / 2,
-		dir_y: (Math.round(Math.random() * 100) % 2 != 1) ? -1 : 1,
-		dir_x: (Math.round(Math.random() * 100) % 2 != 1) ? -1 : 1,
-		speed: 8,
-		out: 0
-	}
-	return (ball);
-}
-
-function actualizeBall(canvas, ball)
-{
-	console.log("Point!");
-	ball.x = canvas.width / 2;
-	ball.y = canvas.height / 2;
-	ball.dir_y = (Math.round(Math.random() * 100) % 2 != 1) ? -1 : 1;
-	ball.dir_x = (Math.round(Math.random() * 100) % 2 != 1) ? -1 : 1;
-	ball.out = 0;
-}
-
 function enableMove(event, paddles)
 {
-	if (event.key == "w")
+	if (event.key == "w" || event.key == "W")
 		paddles.left.move_top = 1;
-	else if (event.key == "s")
+	else if (event.key == "s" || event.key == "S")
 		paddles.left.move_bot = 1;
 	else if (event.key == "ArrowUp")
 		paddles.right.move_top = 1;
@@ -75,9 +53,9 @@ function enableMove(event, paddles)
 
 function disableMove(event, paddles)
 {
-	if (event.key == "w")
+	if (event.key == "w" || event.key == "W")
 		paddles.left.move_top = 0;
-	else if (event.key == "s")
+	else if (event.key == "s" || event.key == "S")
 		paddles.left.move_bot = 0;
 	else if (event.key == "ArrowUp")
 		paddles.right.move_top = 0;
@@ -88,86 +66,132 @@ function disableMove(event, paddles)
 function movePaddles(canvas, paddles)
 {
 	if (paddles.left.move_top == 1 && paddles.left.y > 0)
-		paddles.left.y -= paddles.left.speed;
-	if (paddles.left.move_bot == 1 && paddles.left.y < (canvas.height - paddles.left.height))
-		paddles.left.y += paddles.left.speed;
+		paddles.left.y -= paddles.speed;
+	if (paddles.left.move_bot == 1 && paddles.left.y < (canvas.height - paddles.height))
+		paddles.left.y += paddles.speed;
 	if (paddles.right.move_top == 1 && paddles.right.y > 0)
-		paddles.right.y -= paddles.right.speed;
-	if (paddles.right.move_bot == 1 && paddles.right.y < (canvas.height - paddles.right.height))
-		paddles.right.y += paddles.right.speed;
+	{
+		paddles.right.y -= paddles.speed;
+		console.log(paddles.right);
+	}
+	if (paddles.right.move_bot == 1 && paddles.right.y < (canvas.height - paddles.height))
+	{
+		paddles.right.y += paddles.speed;
+		console.log(paddles.right);
+	}
 }
 
-// si la balle est inferieur ou egale a x paddle gauche et superieur ou egale a padle top y et  einferieur ou egale a paddle top - height paddle ou balle est morte laisser la balle FAIRE LE REBOND sinon AVANCER
-// si balle de x - radius <= paddle de x + width paddle
-// {
-// 	si inoaddle()
-// 		REBOND
-// 	ELSE
-// 		MORT
-// }
+function getBall(canvas)
+{
+	const ball = {
+		radius: 16,
+		x: canvas.width / 2,
+		y: canvas.height / 2,
+		dir_y: 1,
+		dir_x: 1,
+		// dir_y: (Math.round(Math.random() * 100) % 2 != 1) ? -1 : 1,
+		// dir_x: (Math.round(Math.random() * 100) % 2 != 1) ? -1 : 1,
+		speed: 8,
+		out: 0
+	}
+	return (ball);
+}
+
+function resetBall(canvas, ball)
+{
+	console.log("Point!");
+	ball.x = canvas.width / 2;
+	ball.y = canvas.height / 2;
+	ball.dir_y = 1,
+	ball.dir_x = 1,
+	// ball.dir_y = (Math.round(Math.random() * 100) % 2 != 1) ? -1 : 1;
+	// ball.dir_x = (Math.round(Math.random() * 100) % 2 != 1) ? -1 : 1;
+	ball.out = 0;
+}
+
+function collision(paddles, ball)
+{
+	if (ball.x >= (paddles.left.x + paddles.width) && ball.x <= paddles.right.x) // FROM SIDE
+	{
+		if ((ball.x - ball.radius) <= (paddles.left.x + paddles.width)
+			&& ball.y >= paddles.left.y && ball.y <= (paddles.left.y + paddles.height)) // Touche la surface droite du paddle gauche
+		{
+			ball.dir_x *= -1;
+			ball.x += ball.speed * ball.dir_x;
+			ball.y += ball.speed * ball.dir_y;
+		}
+		else if ((ball.x + ball.radius) >= paddles.right.x  && ball.y >= paddles.right.y
+			&& ball.y <= (paddles.right.y + paddles.height)) // Touche la surface gauche du paddle droit
+		{
+			ball.dir_x *= -1;
+			ball.x += ball.speed * ball.dir_x;
+			ball.y += ball.speed * ball.dir_y;
+		}
+	}
+	else if (ball.dir_y > 0) // FROM TOP
+	{
+		if (ball.y < paddles.left.y && (ball.y + ball.radius) >= paddles.left.y) // Juste au dessus du paddle gauche
+		{
+			if (ball.x >= paddles.left.x && ball.x <= (paddles.left.x + paddles.width)) // Dans la surface
+			{
+				ball.dir_y *= -1;
+				ball.dir_x *= -1;
+			}
+		}
+		else if (ball.y < paddles.right.y && (ball.y + ball.radius) >= paddles.right.y) // Juste au dessus du paddle droit
+		{
+			if (ball.x <= (paddles.right.x + paddles.width) && ball.x >= paddles.right.x) // Dans la surface
+			{
+				ball.dir_y *= -1;
+				ball.dir_x *= -1;
+			}
+		}
+	}
+	else if (ball.dir_y < 0) // FROM BOTTOM
+	{
+		if (ball.y > (paddles.left.y + paddles.height) && (ball.y - ball.radius) <= (paddles.left.y + paddles.height)) // Juste en dessous du paddle gauche
+		{
+			if (ball.x >= paddles.left.x && ball.x <= paddles.left.x + paddles.width) // Dans la surface
+			{
+				ball.dir_y *= -1;
+				ball.dir_x *= -1;
+			}
+		}
+		else if (ball.y > (paddles.right.y + paddles.height) && (ball.y - ball.radius) <= (paddles.right.y + paddles.height)) // Juste en dessous du paddle droit
+		{
+			if (ball.x >= paddles.right.x && ball.x <= paddles.right.x + paddles.width) // Dans la surface
+			{
+				ball.dir_y *= -1;
+				ball.dir_x *= -1;
+			}
+		}
+	}
+}
 
 function moveBall(canvas, paddles, ball)
 {
-	if (ball.out == 1 || (ball.x - ball.radius) <= paddles.left.x || ball.x >= (paddles.right.x + paddles.right.width))
-	{
-		ball.out = 1;
-		if (ball.x + (ball.radius * 2) <= 0 || (ball.x - (ball.radius * 2)) >= canvas.width)
-			actualizeBall(canvas, ball);
-	}
-	if (ball.out == 0)
-	{
-		if (ball.y <= ball.radius || ball.y >= (canvas.height - ball.radius))
-			ball.dir_y *= -1;
-		if ((ball.x - ball.radius) <= (paddles.left.x + paddles.left.width) && (ball.y + ball.radius) >= paddles.left.y && (ball.y - ball.radius) <= (paddles.left.y + paddles.left.height))
-		{
-			ball.dir_x *= -1;
-			ball.dir_y *= -1;
-			ball.x += ball.speed * ball.dir_x;
-			ball.y += ball.speed * ball.dir_y;
-		}
-		else if ((ball.x + ball.radius) >= paddles.right.x && (ball.y + ball.radius) >= paddles.right.y && (ball.y + ball.radius) <= (paddles.right.y + paddles.right.height))
-		{
-			ball.dir_x *= -1;
-			ball.dir_y *= -1;
-			ball.x += ball.speed * ball.dir_x;
-			ball.y += ball.speed * ball.dir_y;
-		}
-		// if (ball.x <= (paddles.left.x + paddles.left.width + ball.radius))
-		// {
-		// 	if (ball.dir_y < 0 && (ball.y + ball.radius) >= paddles.left.y && ball.y <= (paddles.left.y + paddles.left.height)
-		// 		|| ball.dir_y > 0 && (ball.y - ball.radius) >= paddles.left.y && ball.y <= (paddles.left.y + paddles.left.height))
-		// 	{
-		// 		ball.dir_x *= -1;
-		// 		ball.dir_y *= -1;
-		// 	}
-		// }
-		// if (ball.x >= (paddles.right.x - ball.radius))
-		// {
-		// 	if (ball.dir_y < 0 && (ball.y + ball.radius) >= paddles.right.y && ball.y <= (paddles.right.y + paddles.right.height)
-		// 		|| ball.dir_y > 0 && (ball.y - ball.radius) >= paddles.right.y && ball.y <= (paddles.right.y + paddles.right.height))
-		// 	{
-		// 		ball.dir_x *= -1;
-		// 		ball.dir_y *= -1;
-		// 	}
-		// }
-	}
+	if (ball.x <= -100 || ball.x >= (canvas.width + 100)) // Out
+		resetBall(canvas, ball);
+	else if (ball.y <= ball.radius || ball.y >= (canvas.height - ball.radius)) // Walls
+		ball.dir_y *= -1;
+	collision(paddles, ball);
 	ball.x += ball.speed * ball.dir_x;
 	ball.y += ball.speed * ball.dir_y;
 }
 
-function display(canvas, ctx, paddles, ball)
+function loop(canvas, ctx, paddles, ball)
 {
 	movePaddles(canvas, paddles);
 	moveBall(canvas, paddles, ball);
 	ctx.fillStyle = "black";
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 	ctx.fillStyle = "white";
-	ctx.fillRect(paddles.left.x, paddles.left.y, paddles.left.width, paddles.left.height);
-	ctx.fillRect(paddles.right.x, paddles.right.y, paddles.right.width, paddles.right.height);
+	ctx.fillRect(paddles.left.x, paddles.left.y, paddles.width, paddles.height);
+	ctx.fillRect(paddles.right.x, paddles.right.y, paddles.width, paddles.height);
 	ctx.beginPath();
 	ctx.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI);
 	ctx.fill();
-	requestAnimationFrame(() => display(canvas, ctx, paddles, ball));
+	requestAnimationFrame(() => loop(canvas, ctx, paddles, ball));
 }
 
 function startPong()
@@ -182,7 +206,7 @@ function startPong()
 	canvas.addEventListener("keyup", function(event) {
 		disableMove(event, paddles);
 	});
-	display(canvas, ctx, paddles, ball);
+	loop(canvas, ctx, paddles, ball);
 }
 
 startPong();

26b1234d505ca4f76c5a3cd4e5d5cd1b4f474f1b
Fixe <Alexioos@hotmail.fr>
2024-05-31 17:17:10 +0200

    Refactorized the .js file.

--- js/pong.js
+++ js/pong.js
@@ -1,53 +1,65 @@
 /////////////////////////
-// Canvas
+// Script
 /////////////////////////
-function	getCanvas()
+startPong();
+
+function	startPong()
 {
-	const canvas = document.getElementById("canvas");
-	return (canvas);
+	let end = 0;
+	const canvas = getCanvas();
+	const ctx = canvas.getContext("2d");
+	const paddles = getPaddles(canvas);
+	const ball = getBall(canvas);
+	canvas.addEventListener("keydown", function(event) {
+		enableMove(event, paddles);
+	});
+	canvas.addEventListener("keyup", function(event) {
+		disableMove(event, paddles);
+	});
+	loop(canvas, ctx, paddles, ball, end);
 }
 
-/////////////////////////
-// Paddles
-/////////////////////////
-function	createPaddle(canvas, height, width, position)
+function	loop(canvas, ctx, paddles, ball, end)
 {
-	let paddle = {
-		x: 0,
-		y: 0,
-		move_top: 0,
-		move_bot: 0,
-		score: 19
-	}
-	if (position == "l")
+	ctx.fillStyle = "#2F2F2F";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	if (end == 0)
 	{
-		paddle.x = canvas.width / 20;
-		paddle.y = (canvas.height / 2) - (height / 2);
+		movePaddles(canvas, paddles);
+		moveBall(canvas, ctx, paddles, ball);
 	}
-	else if (position == "r")
-	{
-		paddle.x = canvas.width - (canvas.width / 20) - width;
-		paddle.y = (canvas.height / 2) - (height / 2);
-	}
-	return (paddle);
-}
-
-function	getPaddles(canvas)
-{
-	const height = canvas.height / 5;
-	const width = canvas.width / 50;
-	let paddles = {
-		height: height,
-		width: width,
-		speed: 10,
-		left: createPaddle(canvas, height, width, "l"),
-		right: createPaddle(canvas, height, width, "r")
-	}
-	return (paddles);
+	render(canvas, ctx, paddles, ball, end);
+	requestAnimationFrame(() => loop(canvas, ctx, paddles, ball, end));
 }
 
 /////////////////////////
-// Paddles - Movements
+// Render
+/////////////////////////
+function	render(canvas, ctx, paddles, ball, end)
+{
+	ctx.fillStyle = "#FDFDFD";
+	ctx.beginPath();
+	ctx.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI);
+	ctx.fill();
+	ctx.stroke();
+	ctx.fillRect(paddles.left.x, paddles.left.y, paddles.width, paddles.height);
+	ctx.fillRect(paddles.right.x, paddles.right.y, paddles.width, paddles.height);
+	renderScore(canvas, ctx, paddles);
+}
+
+function	renderScore(canvas, ctx, paddles)
+{
+	const pictures = getPictures(paddles);
+	if (pictures[0] != 0)
+		ctx.drawImage(pictures[0], ((((canvas.width / 2) / 2) - pictures[1].width) - 5), 20);
+	ctx.drawImage(pictures[1], ((canvas.width / 2) / 2) + 3, 20);
+	if (pictures[2] != 0)
+		ctx.drawImage(pictures[2], (canvas.width / 2) + ((((canvas.width / 2) / 2) - pictures[3].width) - 5), 20);
+	ctx.drawImage(pictures[3], (canvas.width / 2) + ((canvas.width / 2) / 2) - 3, 20);
+}
+
+/////////////////////////
+// Animation
 /////////////////////////
 function	enableMove(event, paddles)
 {
@@ -85,26 +97,13 @@ function	movePaddles(canvas, paddles)
 		paddles.right.y += paddles.speed;
 }
 
-/////////////////////////
-// Ball
-/////////////////////////
-function	generateDirY()
+function	moveBall(canvas, ctx, paddles, ball)
 {
-	const sign = (Math.round(Math.random() * 100) % 2 != 1) ? -1 : 1;
-	return (Math.random() * sign);
-}
-
-function	getBall(canvas)
-{
-	let ball = {
-		radius: 15,
-		x: canvas.width / 2,
-		y: canvas.height / 2,
-		dir_x: (Math.round(Math.random() * 100) % 2 != 1) ? -1 : 1,
-		dir_y: generateDirY(),
-		speed: 10,
-	}
-	return (ball);
+	if (ball.x <= -100 || ball.x >= (canvas.width + 100)) // OUT
+		point(canvas, paddles, ball);
+	else
+		collision(paddles, ball);
+	movePXbyPX(ctx, ball);
 }
 
 function	movePXbyPX(ctx, ball)
@@ -115,49 +114,94 @@ function	movePXbyPX(ctx, ball)
 		ctx.fillStyle = "#2E2E2E";
 		ctx.beginPath();
 		ctx.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI);
-		ctx.fill();
 		ctx.stroke();
-		ball.x += 1 * ball.dir_x;
-		ball.y += 1 * ball.dir_y;
+		ball.x += ball.dir_x;
+		ball.y += ball.dir_y;
 		ctx.fillStyle = "#FDFDFD";
 		ctx.beginPath();
 		ctx.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI);
-		ctx.fill();
 		i++;
 	}
 }
 
-function	moveBall(canvas, ctx, paddles, ball)
+/////////////////////////
+// Point
+/////////////////////////
+function	point(canvas, paddles, ball)
 {
-	if (ball.x <= -100 || ball.x >= (canvas.width + 100)) // OUT
-		point(canvas, paddles, ball);
-	collision(paddles, ball);
-	movePXbyPX(ctx, ball);
+	updateScore(canvas, paddles, ball);
+	resetBall(canvas, ball);
+}
+
+function	updateScore(canvas, paddles, ball)
+{
+	if (ball.x <= -100 && paddles.right.score < 99)
+		paddles.right.score++;
+	else if (ball.x >= (canvas.width + 100) && paddles.left.score < 99)
+		paddles.left.score++;
+}
+
+function	resetBall(canvas, ball)
+{
+	ball.x = canvas.width / 2;
+	ball.y = canvas.height / 2;
+	ball.dir_x = (Math.round(Math.random() * 100) % 2 != 1) ? -1 : 1;
+	ball.dir_y = generateDirY();
 }
 
 /////////////////////////
-// Collisions
+// Collision
 /////////////////////////
+function	collision(paddles, ball)
+{
+	if (ball.y <= ball.radius || ball.y >= (canvas.height - ball.radius)) // WALL
+		ball.dir_y *= -1;
+	else if (ball.x >= (paddles.left.x + paddles.width) && ball.x <= paddles.right.x) // FROM SIDE
+		collisionSide(paddles, ball);
+	else if (ball.dir_y > 0) // FROM TOP
+		collisionTop(paddles, ball);
+	else if (ball.dir_y < 0) // FROM BOTTOM
+		collisionBot(paddles, ball);
+}
+
+function	collisionSide(paddles, ball)
+{
+	if (ball.dir_x < 0 && (ball.x - ball.radius) <= (paddles.left.x + paddles.width)
+	&& ball.y >= paddles.left.y && ball.y <= (paddles.left.y + paddles.height)) // TOUCH SURFACE LEFT PADDLE
+		angle(paddles.left.y, paddles.height, ball);
+	else if (ball.dir_x > 0 && (ball.x + ball.radius) >= paddles.right.x
+	&& ball.y >= paddles.right.y && ball.y <= (paddles.right.y + paddles.height)) // TOUCH SURFACE RIGHT PADDLE
+		angle(paddles.right.y, paddles.height, ball);
+	else
+		collisionCorner(paddles, ball);
+}
+
+function angle(paddleStartY, height, ball)
+{
+	const len  = height / 3;
+	const edge = len / 3;
+	const fromTop = ball.y - paddleStartY;
+	const fromBot = ball.y - (paddleStartY + height);
+	if (fromTop >= 0 && fromTop <= len) // UPPER PART
+	{
+		if (fromTop >= 0 && fromTop <= edge) // TOP
+			ball.dir_y += fromBot / 100;
+		else
+			ball.dir_y -= fromTop / 100;
+	}
+	else if (fromTop >= (len * 2) + 1 && fromTop <= height) // LOWER PART
+	{
+		if (fromTop > ((len * 2) + edge)) // BOTTOM
+			ball.dir_y += fromTop / 100;
+		else
+			ball.dir_y -= fromBot / 100;
+	}
+	ball.dir_x *= -1;
+}
+
 function	collisionCorner(paddles, ball)
 {
-	if (ball.dir_x > 0)
-	{
-		if ((ball.y - ball.radius) <= (paddles.right.y + paddles.height) && ball.y >= (paddles.right.y + paddles.height)
-			&& (ball.x + ball.radius) >= paddles.right.x && ball.x <= paddles.right.x) // BOTTOM LEFT CORNER OF RIGHT PADDLE
-		{
-			ball.dir_x *= -1;
-			if (ball.dir_y < 0)
-				ball.dir_y *= -1;
-		}
-		else if ((ball.y + ball.radius) >= paddles.right.y && ball.y <= paddles.right.y
-			&& (ball.x + ball.radius) >= paddles.right.x && ball.x <= paddles.right.x) // TOP LEFT CORNER OF RIGHT PADDLED
-		{
-			ball.dir_x *= -1;
-			if (ball.dir_y > 0)
-				ball.dir_y *= -1;
-		}
-	}
-	else if (ball.dir_x < 0)
+	if (ball.dir_x < 0) // GOING LEFT
 	{
 		if ((ball.y - ball.radius) <= (paddles.left.y + paddles.height) && ball.y >= (paddles.left.y + paddles.height)
 			&& (ball.x - ball.radius) <= (paddles.left.x + paddles.width) && ball.x >= (paddles.left.x + paddles.width)) // BOTTOM RIGHT CORNER OF LEFT PADDLE
@@ -174,18 +218,23 @@ function	collisionCorner(paddles, ball)
 				ball.dir_y *= -1;
 		}
 	}
-}
-
-function	collisionSide(paddles, ball)
-{
-	if (ball.dir_x < 0 && (ball.x - ball.radius) <= (paddles.left.x + paddles.width)
-	&& ball.y >= paddles.left.y && ball.y <= (paddles.left.y + paddles.height)) // TOUCH SURFACE LEFT PADDLE
-		angle(paddles.left.y, paddles.height, ball);
-	else if (ball.dir_x > 0 && (ball.x + ball.radius) >= paddles.right.x
-	&& ball.y >= paddles.right.y && ball.y <= (paddles.right.y + paddles.height)) // TOUCH SURFACE RIGHT PADDLE
-		angle(paddles.right.y, paddles.height, ball);
-	else
-		collisionCorner(paddles, ball);
+	else if (ball.dir_x > 0) // GOING RIGHT
+	{
+		if ((ball.y - ball.radius) <= (paddles.right.y + paddles.height) && ball.y >= (paddles.right.y + paddles.height)
+			&& (ball.x + ball.radius) >= paddles.right.x && ball.x <= paddles.right.x) // BOTTOM LEFT CORNER OF RIGHT PADDLE
+		{
+			ball.dir_x *= -1;
+			if (ball.dir_y < 0)
+				ball.dir_y *= -1;
+		}
+		else if ((ball.y + ball.radius) >= paddles.right.y && ball.y <= paddles.right.y
+			&& (ball.x + ball.radius) >= paddles.right.x && ball.x <= paddles.right.x) // TOP LEFT CORNER OF RIGHT PADDLED
+		{
+			ball.dir_x *= -1;
+			if (ball.dir_y > 0)
+				ball.dir_y *= -1;
+		}
+	}
 }
 
 function	collisionTop(paddles, ball)
@@ -216,173 +265,98 @@ function	collisionBot(paddles, ball)
 	}
 }
 
-function	collision(paddles, ball)
-{
-	if (ball.y <= ball.radius || ball.y >= (canvas.height - ball.radius)) // WALL
-		ball.dir_y *= -1;
-	else if (ball.x >= (paddles.left.x + paddles.width) && ball.x <= paddles.right.x) // FROM SIDE
-		collisionSide(paddles, ball);
-	else if (ball.dir_y > 0) // FROM TOP
-		collisionTop(paddles, ball);
-	else if (ball.dir_y < 0) // FROM BOTTOM
-		collisionBot(paddles, ball);
-}
-
-function angle(paddleStartY, height, ball)
-{
-	const len  = height / 3;
-	const extreme = len / 3;
-	const fromTop = ball.y - paddleStartY;
-	const fromBot = ball.y - (paddleStartY + height);
-	if (fromTop >= 0 && fromTop <= len) // UPPER PART
-	{
-		if (fromTop >= 0 && fromTop <= extreme) // TOP
-			ball.dir_y += fromBot / 100;
-		else
-			ball.dir_y -= fromTop / 100;
-	}
-	else if (fromTop >= (len * 2) + 1 && fromTop <= height) // LOWER PART
-	{
-		if (fromTop > ((len * 2) + extreme)) // BOTTOM
-			ball.dir_y += fromTop / 100;
-		else
-			ball.dir_y -= fromBot / 100;
-	}
-	ball.dir_x *= -1;
-}
-
 /////////////////////////
-// Scores
+// Getters
 /////////////////////////
-function	getNumbers(paddles)
+function	getCanvas()
 {
-	if (paddles.left.score > 99)
-		paddles.left.score = 99;
-	if (paddles.right.score > 99)
-		paddles.right.score = 99;
-	let numbers = {
-		nb1Left: Math.floor(paddles.left.score / 10),
-		nb2Left: Math.floor(paddles.left.score % 10),
-		nb1Right: Math.floor(paddles.right.score / 10),
-		nb2Right: Math.floor(paddles.right.score % 10)
-	}
-	return (numbers);
+	const canvas = document.getElementById("canvas");
+	return (canvas);
 }
 
-function	getPictures(numbers)
+function	getPaddles(canvas)
 {
-	const pictures = {
-		nb1Left: 0,
-		nb2Left: 0,
-		nb1Right: 0,
-		nb2Right: 0,
+	const height = canvas.height / 5;
+	const width = canvas.width / 50;
+	let paddles = {
+		height: height,
+		width: width,
+		speed: 10,
+		left: createPaddle(canvas, height, width, "l"),
+		right: createPaddle(canvas, height, width, "r")
 	}
-	if (numbers.nb1Left != 0)
+	return (paddles);
+}
+
+function	createPaddle(canvas, height, width, position)
+{
+	let paddle = {
+		x: 0,
+		y: 0,
+		move_top: 0,
+		move_bot: 0,
+		score: 0
+	}
+	if (position == "l")
 	{
-		pictures.nb1Left = new Image();
-		pictures.nb1Left.src = "../svg/" + numbers.nb1Left + ".svg";
+		paddle.x = canvas.width / 20;
+		paddle.y = (canvas.height / 2) - (height / 2);
 	}
-	pictures.nb2Left = new Image();
-	pictures.nb2Left.src = "../svg/" + numbers.nb2Left + ".svg";
-	if (numbers.nb1Right != 0)
+	else if (position == "r")
 	{
-		pictures.nb1Right = new Image();
-		pictures.nb1Right.src = "../svg/" + numbers.nb1Right + ".svg";
+		paddle.x = canvas.width - (canvas.width / 20) - width;
+		paddle.y = (canvas.height / 2) - (height / 2);
 	}
-	pictures.nb2Right = new Image();
-	pictures.nb2Right.src = "../svg/" + numbers.nb2Right + ".svg";
+	return (paddle);
+}
+
+function	getBall(canvas)
+{
+	let ball = {
+		radius: 15,
+		x: canvas.width / 2,
+		y: canvas.height / 2,
+		dir_x: (Math.round(Math.random() * 100) % 2 != 1) ? -1 : 1,
+		dir_y: generateDirY(),
+		speed: 10
+	}
+	return (ball);
+}
+
+function	generateDirY()
+{
+	const sign = (Math.round(Math.random() * 100) % 2 != 1) ? -1 : 1;
+	return (Math.random() * sign);
+}
+
+function	getPictures(paddles)
+{
+	const numbers = getNumbers(paddles);
+	let pictures = [0, 0, 0, 0];
+	if (numbers[0] != 0)
+	{
+		pictures[0] = new Image();
+		pictures[0].src = "../svg/" + numbers[0] + ".svg";
+	}
+	pictures[1] = new Image();
+	pictures[1].src = "../svg/" + numbers[1] + ".svg";
+	if (numbers[2] != 0)
+	{
+		pictures[2] = new Image();
+		pictures[2].src = "../svg/" + numbers[2] + ".svg";
+	}
+	pictures[3] = new Image();
+	pictures[3].src = "../svg/" + numbers[3] + ".svg";
 	return (pictures);
 }
 
-/////////////////////////
-// Point
-/////////////////////////
-function	resetBall(canvas, ball)
+function	getNumbers(paddles)
 {
-	ball.x = canvas.width / 2;
-	ball.y = canvas.height / 2;
-	ball.dir_x = (Math.round(Math.random() * 100) % 2 != 1) ? -1 : 1;
-	ball.dir_y = generateDirY();
+	let numbers = [
+		Math.floor(paddles.left.score / 10),
+		Math.floor(paddles.left.score % 10),
+		Math.floor(paddles.right.score / 10),
+		Math.floor(paddles.right.score % 10)
+	];
+	return (numbers);
 }
-
-function	updateScore(canvas, paddles, ball)
-{
-	if (ball.x <= -100)
-		paddles.right.score++;
-	else if (ball.x >= (canvas.width + 100))
-		paddles.left.score++;
-}
-
-function	point(canvas, paddles, ball)
-{
-	console.log("Point!");
-	updateScore(canvas, paddles, ball);
-	resetBall(canvas, ball);
-}
-
-/////////////////////////
-// Script
-/////////////////////////
-function	renderScore(canvas, ctx, paddles)
-{
-	const numbers = getNumbers(paddles);
-	const pictures = getPictures(numbers);
-	if (numbers.nb1Left != 0)
-		ctx.drawImage(pictures.nb1Left, ((canvas.width / 2) / 2) - 50, 20);
-	ctx.drawImage(pictures.nb2Left, ((canvas.width / 2) / 2) + 3, 20);
-	if (numbers.nb1Right != 0)
-		ctx.drawImage(pictures.nb1Right, (canvas.width / 2) + ((canvas.width / 2) / 2) - 50, 20);
-	ctx.drawImage(pictures.nb2Right, (canvas.width / 2) + ((canvas.width / 2) / 2) - 3, 20);
-}
-
-function	render(canvas, ctx, paddles, ball)
-{
-	ctx.fillStyle = "#FDFDFD";
-	ctx.beginPath();
-	ctx.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI);
-	ctx.fill();
-	ctx.stroke();
-	ctx.fillRect(paddles.left.x, paddles.left.y, paddles.width, paddles.height);
-	ctx.fillRect(paddles.right.x, paddles.right.y, paddles.width, paddles.height);
-	renderScore(canvas, ctx, paddles);
-}
-
-function	loop(canvas, ctx, paddles, ball)
-{
-	ctx.fillStyle = "#2F2F2F";
-	ctx.fillRect(0, 0, canvas.width, canvas.height);
-	movePaddles(canvas, paddles);
-	moveBall(canvas, ctx, paddles, ball);
-	render(canvas, ctx, paddles, ball);
-	requestAnimationFrame(() => loop(canvas, ctx, paddles, ball));
-}
-
-function	startPong()
-{
-	const canvas = getCanvas();
-	const ctx = canvas.getContext("2d");
-	const paddles = getPaddles(canvas);
-	const ball = getBall(canvas);
-	canvas.addEventListener("keydown", function(event) {
-		enableMove(event, paddles);
-	});
-	canvas.addEventListener("keyup", function(event) {
-		disableMove(event, paddles);
-	});
-	// let freeze = 0;
-	// canvas.addEventListener("click", function() {
-	// 	if (freeze == 0)
-	// 	{
-	// 		ball.speed = 0;
-	// 		freeze = 1;
-	// 	}
-	// 	else
-	// 	{
-	// 		ball.speed = 10;
-	// 		freeze = 0;
-	// 	}
-	// });
-	loop(canvas, ctx, paddles, ball);
-}
-
-startPong();

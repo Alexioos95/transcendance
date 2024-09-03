82a5501cc0da612ca7dfe4afa687e0e6664c70de
mathieu <mathieu.daguenet@outlook.com>
2024-09-03 19:11:12 +0200

    1 km a pied ca use les soulliers

--- retourVm/transcendance/services/User/transcendance/transcendance/urls.py
+++ retourVm/transcendance/services/User/transcendance/transcendance/urls.py
@@ -19,9 +19,10 @@ from django.urls import path
 from user import views
 
 #data user
-# {"2fa": 'False', "username":"username", "Avatar":PATH, "Language": "FR"}//ajouter les amis et bloques
+# {"2fa": 'False', "username":"username", "avatar":PATH, "language": "FR", friends:[], blockedUser:[]}//ajouter les amis et bloques
 #ajouter le type de methode sur chaqaue route
 #code 2fa valide 10 minutes?
+#tout les echanges client serveur se font en json
 
 urlpatterns = [
     path('', views.index),#faire une route static via nginx a enlever d'ici
@@ -34,24 +35,27 @@ urlpatterns = [
     #non publique redirecion du line 42
     path('user/auth42/', views.auth42),
     #POST
-    path('user/2fa/', views.twoFA),#envoyer un code user qu'il a obtenu via son moyen de double authentification retourn 200 + # {"username":"username", "Avatar":PATH, "Language": "FR"} si le code est valide
+    path('user/2fa/', views.twoFA), #envoyer un code user qu'il a obtenu via son moyen de double authentification retourn 200 + # {"username":"username", "Avatar":PATH, "Language": "FR"} si le code est valide
     #POST
-    path('user/set2FA/', views.set2FA),#envoi le type de 2fa voulu si mail un code sera envoye au mail et le retour sera 200 si ca c'est corectement passe (403 si invalide user) et un code sera attendu en retour via la meme route qui le settera la 2fa si le code est bon sinon code d'erreur et 2fa non set 
+    path('user/set2FA/', views.set2FA), #envoi le type de 2fa voulu si mail un code sera envoye au mail et le retour sera 200 si ca c'est corectement passe (403 si invalide user) et un code sera attendu en retour via la meme route qui le settera la 2fa si le code est bon sinon code d'erreur et 2fa non set 
     #GET
-    path('user/disconnect/', views.disconnect),#supprime le coockie auth
+    path('user/disconnect/', views.disconnect), #supprime le coockie auth
     #GET
-    path('user/checkAuth42/', views.checkAuth42),#ping a intervalle reguliere pour savoir si la connection 42 a reussi a ne pas ajouter a la doc retourne 200 et les infos de l'user si correct
+    path('user/checkAuth42/', views.checkAuth42), #ping a intervalle reguliere pour savoir si la connection 42 a reussi a ne pas ajouter a la doc retourne 200 et les infos de l'user si correct
     #POST
-    path('user/updateUserInfos/', views.updateUserInfos),#envoyer les infos a modifer pour l'user sauf 2fa
-    #POST envoi un username a jouter a une des liste ami bloque
+    path('user/updateUserInfos/', views.updateUserInfos), #envoyer les infos a modifer pour l'user sauf 2fa
+    # POST envoi un username a jouter a une des liste ami bloque
     # path('user/addFriend/', views.addFriend),en attene de postgres
     # path('user/blockUser/', views.blockUser),en attene de postgres
     #POST
-    path('user/resetPaswd/', views.resetPasswd),#envoi un username auquel sera envoye un mail de reset de son password via un lien fourni
+    path('user/resetPaswd/', views.resetPasswd), #envoi un username auquel sera envoye un mail de reset de son password via un lien fourni
     #GET
-    path('user/matchMaking/', views.matchMaking),#a ping regulierement renverra 403 si token invalide une 100 si la recherche et en cours une 200 si un adveraire a ete trouve il faudra ensuite se connecter a la socket du jeu
+    path('user/matchMaking/', views.matchMaking), #a ping regulierement renverra 403 si token invalide une 100 si la recherche et en cours une 200 si un adveraire a ete trouve il faudra ensuite se connecter a la socket du jeu
     #GET
-    path('user/updateInfo/', views.updateInfo),#requete a effetuer regulierement (1s?) afin de recevoir les defis/acceptation de defi liste d'ami avec leur date de connection liste de bloque a jour {defi:"nom du jeu si defi accepte ping la route du jeu sinon vide", friendList:[], blockedList:[]}
+    path('user/updateInfo/', views.updateInfo), #requete a effetuer regulierement (1s?) afin de recevoir les defis/acceptation de defi liste d'ami avec leur date de connection liste de bloque a jour {defi:"nom du jeu si defi accepte ping la route du jeu sinon vide", friendList:[], blockedList:[]}
+    #GET
+    ping('user/ping/', views.ping), #renvoi 204 si ok sinon vtf
+
 ]
 
 # route matchmaking servie a part ou service user

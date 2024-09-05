72c6642375d081b9c6e7fe25bc47b44d2a50bb2c
Fabrice GUARRACI <fguarrac@student.42.fr>
2024-09-05 13:52:09 +0200

    Merged MailServ to services + added persistent volume to Webserver

---
+++ retourVm/transcendance/services/MailServ/transcendance/MailServ/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MailservConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'MailServ'
---
+++ retourVm/transcendance/services/Webserver/static/js/index.js
@@ -0,0 +1,64 @@
+"use strict";
+/////////////////////////
+// Script
+/////////////////////////
+checkJWT();
+
+async function	checkJWT()
+{
+	// PING ALL MODULES
+	console.log("fetch /user/checkJwt");
+	await fetch("/user/checkJwt/")
+		.then(response => {
+			if (response.ok)
+			{
+				console.log("response /user/checkJwt ok; navigate to Game");
+				navigate("game", JSON.parse(response.json()));
+			}
+			else
+			{
+				console.log("response /user/checkJwt not good; navigate to login");
+				console.log("response.status=", response.status);
+				console.log("response.text=", response.text());
+				navigate("login", undefined);
+			}
+		})
+		.catch(() => console.error("Error: failed to fetch the checkJwt route"))
+}
+
+// window.onpopstate = function(event) {
+// 	if (event.state)
+// 	{
+// 		if (event.state.login === true)
+// 		{
+// 			navigate("login")
+// 				.then(() => launchPageScript("login", false, event.state.signUp))
+// 				.catch((e) => console.log(e));
+// 		}
+// 		else if (event.state.game === true)
+// 		{
+// 			navigate("game")
+// 				.then(() => launchPageScript("game", true, false))
+// 				.catch((e) => console.log(e));
+// 		}
+// 	}
+// };
+
+async function navigate(page, data)
+{
+	const container = document.getElementsByTagName("body")[0];
+
+	await fetch("/html/" + page + ".html")
+		.then(response => response.text())
+		.then(html => { container.innerHTML = html; })
+		.then(() => { launchPageScript(page, data) })
+		.catch(() => Promise.reject("Error: couldn't fetch the page"))
+}
+
+async function	launchPageScript(page, data)
+{
+	if (page === "game")
+		run(data);
+	else if (page === "login")
+		login();
+}

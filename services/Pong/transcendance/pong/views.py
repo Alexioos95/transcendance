2b4047e5463d9b60e9630dd6d8a9a6592ebdcf07
Nassim SALHI <nsalhi@bess-f2r3s16.clusters.42paris.fr>
2024-09-10 14:54:32 +0200

    ajout de Pong dans service

---
+++ services/Pong/transcendance/pong/views.py
@@ -0,0 +1,41 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
+from .models import Game
+import json
+
+# Create your views here.
+@csrf_exempt
+def initGame(request):
+    if request.method != 'POST':
+        return JsonResponse({'error': 'Invalid request method'}, status=405)
+    
+    try:
+        data = json.loads(request.body)
+    except json.JSONDecodeError:
+        return JsonResponse({'error': 'Invalid JSON'}, status=400)
+    
+    player1 = data.get('player1')
+    player2 = data.get('player2')
+
+    if player1 is None or player2 is None:
+        print(f'player 2 == {player1} player 2 == {player2}')
+        return JsonResponse({'error': 'player1 and player2 are required'}, status=403)
+    
+    # Créer une nouvelle instance de Game avec la date actuelle
+    game = Game(
+        Player1=player1,
+        Player2=player2,
+        gameDate=timezone.now(), # Utiliser la date et l'heure actuelles
+        gameEnded=False,  # Définir les valeurs par défaut
+        scorePlayer1=0,
+        scorePlayer2=0,
+        winner=''
+    )
+    game.save()
+    
+    return JsonResponse({'status': 'Game created successfully'}, status=201)
+
+def index(request):
+    return render(request, 'index.html')

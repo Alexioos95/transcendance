c7c3b6d81caae54b490ef1ff98cc1c5d970701ec
Fabrice GUARRACI <fguarrac@student.42.fr>
2024-09-12 14:59:11 +0200

    Added Ping route

--- services/Ping/transcendance/ping/views.py
+++ services/Ping/transcendance/ping/views.py
@@ -6,22 +6,39 @@ import requests
 
 def testRoutes(request):
     services = ["user", "chat", "ping", "mail"]
-   # pingResponses = [0] * 4
     pingResponses = {}
-    #index = 0
     for _ in services:
         try:
-            #pingResponses[index] = requests.get('http://' + _ + '/ping').status_code
-            pingResponses[_] = requests.get('http://' + _ + '/ping').status_code
+            pingResponses.append({_: requests.get('http://' + _ + '/ping').status_code})
         except Exception:
-            print("No ping for " + _ + " !")
-            pingResponses[_] = "0"
-        #index = index + 1
+            pingResponses.append({_: 0})
     for _ in pingResponses:
         if pingResponses[_] != 204:
             print("No Ping")
     print(pingResponses)
-    return HttpResponse(status=200)
+	return JsonResponse(pingResponses, status=200)
+
+#definir les routes actives en fonction du retour des pings
+
+#    path('user/register/', views.register),
+#    path('user/login/', views.login),
+#    path('user/checkJwt/', views.checkJwt),
+#  	  path('user/auth42/', views.auth42),
+#    path('user/set2fa/', views.checkCodeSet),
+#    path('user/log2fa/', views.checkCodeLog),
+#    path('user/init2fa/', views.set2FA),
+#    path('user/disconnect/', views.disconnect),
+#    path('user/checkAuth42/', views.checkAuth42),
+#    path('user/updateUserInfos/', views.updateUserInfos),
+#    path('user/addFriend/', views.addFriend),
+#    path('user/blockUser/', views.blockUser),
+#    path('user/deleteFriend/', views.deleteFriend),
+#    path('user/deleteBlockedUser/', views.deleteBlockedUser),
+#    path('user/resetPaswd/', views.resetPasswd),
+#    path('user/sendNewPaswd/', views.sendNewPaswd),
+#    path('user/matchMaking/', views.matchMaking),
+#    path('user/updateInfo/', views.updateInfo),
+#    path('user/ping/', views.ping),
 
 def ping(request):
     HttpResponse(status=204)

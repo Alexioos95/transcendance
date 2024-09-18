e59fd743a9d838da16633f27d951feaa395f6f1b
Fabrice GUARRACI <fguarrac@student.42.fr>
2024-09-18 12:46:48 +0200

    Rmoved useless files

--- services/Webserver/static/js/redir.js
+++
@@ -1,1 +0,0 @@
-https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-f59fbc2018cb22b75560aad5357e1680cd56b1da8404e0155abc804bc0d6c4b9&redirect_uri=http%3A%2F%2Fbess-f2r4s15%3A4433%2Fauth42&response_type=code
